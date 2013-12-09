4d0001dfb1ef18c5f6b42411cff2def7f0dbd15c
haeric <hakon.erichsen@gmail.com>
2013-12-09 20:23:48 +0100

    karsk.js now takes the file as input

--- karsk.js
+++ karsk.js
@@ -2,12 +2,15 @@
 var PEG = require('pegjs');
 var fs = require('fs');
 
+if (process.argv.length !== 3) {
+    console.error('karsk.js takes one argument: the karsk file to process');
+    process.exit(1);
+}
+
 fs.readFile('parser.peg', 'utf8', function(err, parserData) {
-    fs.readFile('example.bs', 'utf8', function (err, input) {
+    fs.readFile(process.argv[2], 'utf8', function (err, input) {
         var parser = PEG.buildParser(parserData);
         var ast = parser.parse(input);
-
-        console.log(JSON.stringify(ast));
 
         for ( var i = 0; i < ast.length; i++ ) {
             console.log(ast[i].toJS());
