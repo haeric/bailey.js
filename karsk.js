dbf12066777191b38da4245a9cef5e4643c6a6de
haeric <hakon.erichsen@gmail.com>
2013-12-09 16:57:57 +0100

    Start work on output pipeline

--- karsk.js
+++ karsk.js
@@ -5,6 +5,14 @@ var fs = require('fs');
 fs.readFile('parser.peg', 'utf8', function(err, parserData) {
     fs.readFile('example.bs', 'utf8', function (err, input) {
         var parser = PEG.buildParser(parserData);
-        console.log(JSON.stringify(parser.parse(input)));
+        var ast = parser.parse(input);
+
+        console.log(JSON.stringify(ast));
+
+        for ( var i = 0; i < ast.length; i++ ) {
+            if (ast[i].toJS) {
+                console.log(ast[i].toJS());
+            }
+        }
     });
 });
