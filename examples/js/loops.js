0cc0ec379223070d8fa5902415c7e97c2d1dc78f
Rolf Erik Lekang <me@rolflekang.com>
2014-05-12 20:53:48 -0800

    Update documentation

---
+++ examples/js/loops.js
@@ -0,0 +1,14 @@
+for (var __i1 = 0; __i1 < bagOfDelicousCandy.length; __i1++) {
+    var item = bagOfDelicousCandy[__i1];
+    console.log(item)
+}
+
+for (var number = 0; number < 10; number = number + 2) {
+    console.log(number)
+}
+
+var chocolateCount = 4;
+while (chocolateCount > 0) {
+    console.log('Eat chocolate')
+    chocolateCount -= 1;
+}
